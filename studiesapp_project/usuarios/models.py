3ce5203f1f57ab5c7c7522897e77fb375837651f
caiiohenryk <crobatmobile@gmail.com>
2024-02-19 14:08:15 -0300

    default user/publications model

--- studiesapp_project/usuarios/models.py
+++ studiesapp_project/usuarios/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 class Usuario(models.Model):
-    email = models.EmailField
+    email = models.EmailField(max_length=255, default='')
     username = models.CharField(max_length=30)
     senha = models.CharField(max_length=100)
-    profilePicture = models.ImageField
+    def __str__(self) -> str:
+        return self.username
+    
+class Publicacoes(models.Model):
+    dataPublicacao = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
+    comentarios = models.IntegerField(default=0)
+    autorPub = models.ForeignKey(Usuario, on_delete=models.CASCADE)
